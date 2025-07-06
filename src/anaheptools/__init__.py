c7a05281858553179c7a67b770574b7aa75fc210
Raul Rabadan <raul.iraq.rabadan.trejo@cern.ch>
2025-07-06 20:29:24 +0200

    Update __all__ in __init__.py to include Var, compute_single_var_array, and arrays_from_vars

--- src/anaheptools/__init__.py
+++ src/anaheptools/__init__.py
@@ -3,9 +3,13 @@
 __version__ = "0.1.0"
 
 from . import histograms, plotting, variables
+from .variables import Var, arrays_from_vars, compute_single_var_array
 
 __all__ = [
     "histograms",
     "plotting",
     "variables",
+    "Var",
+    "compute_single_var_array",
+    "arrays_from_vars",
 ]
